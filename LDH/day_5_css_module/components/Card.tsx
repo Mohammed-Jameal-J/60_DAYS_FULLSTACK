2e1ef66f21a5b8f78a2cbdb1d64c116d6e38a003
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-03-02 19:14:11 +0530

    css-module

---
+++ LDH/day_5_css_module/components/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Card.module.css'
+
+const Card = () => {
+  return (
+    <>
+    <h2 className={styles.card}>Card</h2>
+    <p className={styles.para}>Card Component Page</p>
+    </>
+  )
+}
+
+export default Card
