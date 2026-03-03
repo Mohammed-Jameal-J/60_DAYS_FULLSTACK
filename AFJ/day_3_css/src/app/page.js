420585d22616e6b7a02169bf13fa2e15ac076e6f
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-03-03 15:52:48 +0530

    css

---
+++ AFJ/day_3_css/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import User from "@/component/User";
+
+export default function Home() {
+  return (
+    <User />
+  );
+}
---
+++ AFJ/day_3_css/src/component/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './User.module.css'
+
+const User = () => {
+  return (
+    <h1 className={styles.title}>User Title</h1>
+
+  )
+}
+
+export default User
