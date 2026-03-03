0eb0887c94ee6da2f724018fe92c6d64795bc55f
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-03-03 15:31:59 +0530

    fontandmeta

---
+++ AFJ/day_2_metaandfonts/app/fonts.tsx
@@ -0,0 +1,14 @@
+import {Zen_Antique , Anek_Tamil} from "next/font/google"
+
+export const zenAntique = Zen_Antique({
+  variable: "--font-zen-antique",
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400"],
+})
+
+export const anekTamil = Anek_Tamil({
+  variable: "--font-anek-tamil",
+  subsets: ["latin"],
+  display: "swap",
+})
