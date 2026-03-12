aecf1855b03ab0d300af5ce005de555a2560ad2f
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-03-12 23:58:15 +0530

    prisma

---
+++ LDH/11_shopping_app/online-store/prisma/seed.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const connectionString = process.env.DATABASE_URL || "file:./dev.db";
+
+const prisma = new PrismaClient({
+    adapter: new PrismaBetterSqlite3({
+        url: connectionString,
+    }),
+});
+
+async function main() {
+  await prisma.healthCheck.create({
+    data: {
+      message: "Hello, World!",
+    },
+  });
+
+  console.log("Database seeded successfully!");
+}
+
+main().catch((e) => {
+    console.error(e);
+}).finally(async () => {
+    await prisma.$disconnect();
+});
